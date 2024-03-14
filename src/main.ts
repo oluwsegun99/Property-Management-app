d74172fc046f0c1c1da1cf6a215f38cd19fb411a
oluwsegun99 <obafemifavour@gmail.com>
2024-03-14 15:41:12 +0100

    Cors disabled

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    allowedHeaders: ["Content-Type", "Authorization"],
-    origin: [
-      'http://localhost:3000',
-    ],
-    credentials: true,
-    preflightContinue: true,
-    methods: ['GET', 'PUT', 'POST', 'DELETE', 'HEAD', 'PATCH'],
-    optionsSuccessStatus: 200
-  });
+  // app.enableCors({
+  //   allowedHeaders: ["Content-Type", "Authorization"],
+  //   origin: [
+  //     'http://localhost:3000',
+  //   ],
+  //   credentials: true,
+  //   preflightContinue: true,
+  //   methods: ['GET', 'PUT', 'POST', 'DELETE', 'HEAD', 'PATCH'],
+  //   optionsSuccessStatus: 200
+  // });
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(8082);
