32432e98738ade09bb4b35e18445c40be1b73da7
oluwsegun99 <obafemifavour@gmail.com>
2024-03-14 13:26:43 +0100

    Enabled cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    allowedHeaders: ["content-type"],
+    origin: [
+      'http://localhost:3000',
+    ],
+    credentials: true,
+    preflightContinue: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(8082);
 }
