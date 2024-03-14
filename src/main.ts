101ee49b0889b22a686afa3b061178508587328a
oluwsegun99 <obafemifavour@gmail.com>
2024-03-14 14:26:29 +0100

    Cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
       'http://localhost:3000',
     ],
     credentials: true,
-    preflightContinue: true,
+    // preflightContinue: true,
   });
 
   app.useGlobalPipes(new ValidationPipe());
